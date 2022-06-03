c33859a4e34fcdeac47fb849c718b57ea5b68e37
sakib <sakibsyed01@gmail.com>
2022-06-03 21:40:39 +0600

    modal, text input, picker

---
+++ app/components/AppPicker.js
@@ -0,0 +1,90 @@
+import {
+  StyleSheet,
+  TouchableWithoutFeedback,
+  View,
+  Modal,
+  Button,
+  FlatList,
+} from "react-native";
+import React, { useState } from "react";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+
+import colors from "../config/colors";
+import AppText from "./AppText";
+import Screen from "./Screen";
+import PickerItem from "./PickerItem";
+
+const AppPicker = ({
+  icon,
+  placeholder,
+  items,
+  selectedItem,
+  onSelectItem,
+}) => {
+  const [modalVisible, setModalVisible] = useState(false);
+  return (
+    <>
+      <TouchableWithoutFeedback onPress={() => setModalVisible(true)}>
+        <View style={styles.container}>
+          {icon && (
+            <MaterialCommunityIcons
+              name={icon}
+              size={20}
+              color={colors.medium}
+              style={styles.icon}
+            />
+          )}
+          <AppText style={styles.text}>
+            {selectedItem ? selectedItem.label : placeholder}
+          </AppText>
+          <MaterialCommunityIcons
+            name="chevron-down"
+            size={20}
+            color={colors.medium}
+          />
+        </View>
+      </TouchableWithoutFeedback>
+
+      <Modal visible={modalVisible} animationType="slide">
+        <Screen>
+          <Button title="close" onPress={() => setModalVisible(false)} />
+          <FlatList
+            data={items}
+            keyExtractor={(item) => item.value.toString()}
+            renderItem={({ item }) => (
+              <PickerItem
+                label={item.label}
+                onPress={() => {
+                  setModalVisible(false);
+                  onSelectItem(item);
+                }}
+              />
+            )}
+          />
+        </Screen>
+      </Modal>
+    </>
+  );
+};
+
+export default AppPicker;
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: colors.light,
+    borderRadius: 25,
+    flexDirection: "row",
+    width: "100%",
+    padding: 15,
+    marginVertical: 10,
+
+    display: "flex",
+    alignItems: "center",
+  },
+  icon: {
+    marginRight: 10,
+  },
+  text: {
+    flex: 1,
+  },
+});
